96db4290791b4f63e476aaf80bc223c740cc48cb
mickael keita <keitamickael@gmail.com>
2023-02-24 12:30:17 +0100

    style: add responsive header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import logo from "../../assets/images/logo.png";
+
+function Header() {
+  console.log(window.innerWidth);
+  return (
+    <header className="header">
+      <div className="header__info">
+        <div className="container">
+          <ul>
+            <li>
+              <span>
+                <i className="fa-solid fa-phone"></i>
+              </span>
+              01 23 45 67 89
+            </li>
+            <li>
+              <span>
+                <i className="fa-solid fa-envelope"></i>
+              </span>
+              kanap@gmail.com
+            </li>
+            <li>
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+              </span>
+              10 quai de la charente 75019 Paris 19
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="header__logo">
+        <div className="container">
+          <img src={logo} alt="entrprise" />
+          <nav>
+            <ul>
+              <li>Accueil</li>
+              <li>Pannier</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
