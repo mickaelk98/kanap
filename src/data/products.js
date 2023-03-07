48ea806af0dc9cd227ee8740a7467050336d728b
mickael keita <keitamickael@gmail.com>
2023-03-07 12:19:45 +0100

    fix: correct product image paths

--- src/data/products.js
+++ src/data/products.js
@@ -1,10 +1,19 @@
+import productImage1 from "@/assets/images/kanap01.jpeg";
+import productImage2 from "@/assets/images/kanap02.jpeg";
+import productImage3 from "@/assets/images/kanap03.jpeg";
+import productImage4 from "@/assets/images/kanap04.jpeg";
+import productImage5 from "@/assets/images/kanap05.jpeg";
+import productImage6 from "@/assets/images/kanap06.jpeg";
+import productImage7 from "@/assets/images/kanap07.jpeg";
+import productImage8 from "@/assets/images/kanap08.jpeg";
+
 export const products = [
   {
     colors: ["Blue", "White", "Black"],
     _id: "107fb5b75607497b96722bda5b504926",
     name: "Kanap Sinopé",
     price: 1849,
-    imageUrl: `/src/assets/images/kanap01.jpeg`,
+    imageUrl: `${productImage1}`,
     description:
       "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     altTxt: "Photo d'un canapé bleu, deux places",
@@ -14,7 +23,7 @@ export const products = [
     _id: "415b7cacb65d43b2b5c1ff70f3393ad1",
     name: "Kanap Cyllène",
     price: 4499,
-    imageUrl: "/src/assets/images/kanap02.jpeg",
+    imageUrl: `${productImage2}`,
     description:
       "Morbi nec erat aliquam, sagittis urna non, laoreet justo. Etiam sit amet interdum diam, at accumsan lectus.",
     altTxt: "Photo d'un canapé jaune et noir, quattre places",
@@ -24,7 +33,7 @@ export const products = [
     _id: "055743915a544fde83cfdfc904935ee7",
     name: "Kanap Calycé",
     price: 3199,
-    imageUrl: "/src/assets/images/kanap03.jpeg",
+    imageUrl: `${productImage3}`,
     description:
       "Pellentesque fermentum arcu venenatis ex sagittis accumsan. Vivamus lacinia fermentum tortor.Mauris imperdiet tellus ante.",
     altTxt: "Photo d'un canapé d'angle, vert, trois places",
@@ -34,7 +43,7 @@ export const products = [
     _id: "a557292fe5814ea2b15c6ef4bd73ed83",
     name: "Kanap Autonoé",
     price: 1499,
-    imageUrl: "/src/assets/images/kanap04.jpeg",
+    imageUrl: `${productImage4}`,
     description:
       "Donec mattis nisl tortor, nec blandit sapien fermentum at. Proin hendrerit efficitur fringilla. Lorem ipsum dolor sit amet.",
     altTxt: "Photo d'un canapé rose, une à deux place",
@@ -44,7 +53,7 @@ export const products = [
     _id: "8906dfda133f4c20a9d0e34f18adcf06",
     name: "Kanap Eurydomé",
     price: 2249,
-    imageUrl: "/src/assets/images/kanap05.jpeg",
+    imageUrl: `${productImage5}`,
     description:
       "Ut laoreet vulputate neque in commodo. Suspendisse maximus quis erat in sagittis. Donec hendrerit purus at congue aliquam.",
     altTxt: "Photo d'un canapé gris, trois places",
@@ -54,7 +63,7 @@ export const products = [
     _id: "77711f0e466b4ddf953f677d30b0efc9",
     name: "Kanap Hélicé",
     price: 999,
-    imageUrl: "/src/assets/images/kanap06.jpeg",
+    imageUrl: `${productImage6}`,
     description:
       "Curabitur vel augue sit amet arcu aliquet interdum. Integer vel quam mi. Morbi nec vehicula mi, sit amet vestibulum.",
     altTxt: "Photo d'un canapé gris, deux places",
@@ -64,7 +73,7 @@ export const products = [
     _id: "034707184e8e4eefb46400b5a3774b5f",
     name: "Kanap Thyoné",
     price: 1999,
-    imageUrl: "/src/assets/images/kanap07.jpeg",
+    imageUrl: `${productImage7}`,
     description:
       "EMauris imperdiet tellus ante, sit amet pretium turpis molestie eu. Vestibulum et egestas eros. Vestibulum non lacus orci.",
     altTxt: "Photo d'un canapé rouge, deux places",
@@ -74,7 +83,7 @@ export const products = [
     _id: "a6ec5b49bd164d7fbe10f37b6363f9fb",
     name: "Kanap orthosie",
     price: 3999,
-    imageUrl: "/src/assets/images/kanap08.jpeg",
+    imageUrl: `${productImage8}`,
     description:
       "Mauris molestie laoreet finibus. Aenean scelerisque convallis lacus at dapibus. Morbi imperdiet enim metus rhoncus.",
     altTxt: "Photo d'un canapé rose, trois places",
